4a86edf043bf77b6f06e6048daa5593a3fdb6c65
lawsona7 <lawsona7@mail.uc.edu>
2016-11-21 20:08:30 -0500

    First Commit

---
+++ GroceryApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GroceryApp.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public DbSet<Grocery> Groceries { get; set; }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ GroceryApp/Models/Grocery.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GroceryApp.Models
+{
+    public class Grocery
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+        [Required]
+        public DateTime SellBy { get; set; }
+        
+        public string Status()
+        {
+            string used = Done;
+            double expired = (SellBy - DateTime.Now).TotalDays;
+            if(expired >= 0)
+            {
+                return "Good";
+            }
+            else if (expired <= 0 || used == "Yes")
+            {
+                return "Bad";
+            }
+            else
+            {
+                return "Good";
+            }
+        }
+        
+        [Required]
+        public string Done { get; set; }
+
+        [Required]
+        public string Repeat { get; set; }
+        
+        
+        public object DaysToExpiration()
+        {
+            DateTime today = DateTime.Now;
+            DateTime expire = SellBy;
+            double days=(expire - today).TotalDays;
+            if(days >= 0)
+            {
+                return days;
+            }
+            else
+            {
+                return "bad";
+            }
+        }
+        
+
+    }
+}
